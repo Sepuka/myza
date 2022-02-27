f6bec8a0c99fe33849a085d4658b7e9ecbba285b
Maksim Shlomin <m.shlomin@corp.vk.com>
2022-02-27 17:14:37 +0300

    WIP ya oauth

---
+++ def/income/ya_auth.go
@@ -0,0 +1,40 @@
+package income
+
+import (
+	"github.com/sarulabs/di/v2"
+	"github.com/sepuka/myza/def"
+	"github.com/sepuka/myza/def/db"
+	"github.com/sepuka/myza/def/http"
+	"github.com/sepuka/myza/def/log"
+	"github.com/sepuka/myza/internal/config"
+	"github.com/sepuka/vkbotserver/domain"
+	"github.com/sepuka/vkbotserver/message"
+	"go.uber.org/zap"
+	http2 "net/http"
+)
+
+const (
+	AuthYaDef = `def.auth.ya`
+)
+
+func init() {
+	def.Register(func(builder *di.Builder, cfg *config.Config) error {
+		return builder.Add(di.Def{
+			Name: AuthYaDef,
+			Tags: []di.Tag{
+				{
+					Name: ExecutorDef,
+					Args: nil,
+				},
+			},
+			Build: func(ctx di.Container) (interface{}, error) {
+				var (
+					logger     = ctx.Get(log.LoggerDef).(*zap.SugaredLogger)
+					httpClient = ctx.Get(http.ClientDef).(*http2.Client)
+					userRepo   = ctx.Get(db.UserRepoDef).(domain.UserRepository)
+				)
+				return message.NewYaAuth(cfg.Server.YaOauth, httpClient, logger, userRepo), nil
+			},
+		})
+	})
+}
